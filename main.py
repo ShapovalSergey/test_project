8fccb7675e41c1b4c79d3e68407aff62b64c0263
ShapovalSergey <sergoshapo09@gmail.com>
2025-04-18 21:59:16 +0700

    Add coments to some part of code (in english ofc). Add README file

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Core():
         self.status = False
         self.pg_conf_path = ""
         self.install_status=False
-        
+
     def start(self):
         self.scanf()
         hosts_list = list(filter(None, re.split(' |,',self.hosts)))
@@ -47,11 +47,14 @@ class Core():
         sshcon   = paramiko.SSHClient() 
         sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
         sshcon.connect(hostname, username=myuser, pkey=pkey)
+        #Check server load status using top utility
         stdin, stdout, stderr = sshcon.exec_command("cat /proc/loadavg")
+        #Get load of last 1 minute. It is used for determination of less loaded server
         curr_host_load = float(stdout.read().decode(encoding='UTF-8').split()[0])
         if self.min_load > curr_host_load: 
             self.min_load = curr_host_load
             self.fhost = host
+            #Get distribution name to execute right command for postgresql install
             stdin, stdout, stderr = sshcon.exec_command("cat /etc/*-release | grep NAME | head -n1 | cut -d '=' -f2")
             self.fhostdistro = stdout.read().decode(encoding='UTF-8')
         sshcon.close()
@@ -64,6 +67,7 @@ class Core():
         sshcon   = paramiko.SSHClient() 
         sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
         sshcon.connect(hostname, username=myuser, pkey=pkey)
+        #Check posgtgresql.service status
         stdin, stdout, stderr = sshcon.exec_command("systemctl status postgresql.service")
         print(stdout.read().decode(encoding='UTF-8'))
         sshcon.close()
@@ -76,16 +80,22 @@ class Core():
         sshcon   = paramiko.SSHClient() 
         sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
         sshcon.connect(hostname, username=myuser, pkey=pkey)
+        #Allow database to accept connection. listen_addresses replaces with value *, that means allow all connections
         stdin, stdout, stderr = sshcon.exec_command("sed -i \"s|#listen_addresses = 'localhost'|listen_addresses = '*'|\" " + self.pg_conf_path + "postgresql.conf")
         stdout.read().decode(encoding='UTF-8')
+        #Open port to access database
         stdin, stdout, stderr = sshcon.exec_command("sed -i \"s|#port = 5432|port = 5432|\" " + self.pg_conf_path + "postgresql.conf")
         stdout.read().decode(encoding='UTF-8')
+        #Restart postgresql.service to make changes work
         stdin, stdout, stderr = sshcon.exec_command("systemctl restart postgresql.service")
         stdout.read().decode(encoding='UTF-8')
+        #Create database user student with password=password
         stdin, stdout, stderr = sshcon.exec_command("su -c \"psql -c \\\"CREATE ROLE student WITH LOGIN SUPERUSER PASSWORD 'password'\\\"\" postgres")
         stdout.read().decode(encoding='UTF-8')
+        #Append pg_hba.conf file to allow 2nd server (almalinux) connect to all databases using user student
         stdin, stdout, stderr = sshcon.exec_command("echo \"host\t\tall\tstudent\t\t192.168.0.109/32\tmd5\n\" >>" + self.pg_conf_path + "pg_hba.conf")
         stdout.read().decode(encoding='UTF-8')
+        #Check database work ability using SELECT 1 (according to task)
         stdin, stdout, stderr = sshcon.exec_command("su -c \"psql -c \\\"SELECT 1;\\\"\" postgres")
         print("#Database check (SELECT 1)\n\n"+stdout.read().decode(encoding='UTF-8'))
         sshcon.close()
@@ -99,13 +109,18 @@ class Core():
         sshcon.set_missing_host_key_policy(paramiko.AutoAddPolicy()) 
         sshcon.connect(hostname, username=myuser, pkey=pkey)
         if hostdistro.startswith("\"Deb"):
+            #Install postgresql on debian
             stdin, stdout, stderr = sshcon.exec_command("apt install -y postgresql-15 postgresql-contrib")
             stdout.read().decode(encoding='UTF-8')
+            #Write path to conf files in variables for debian
             self.pg_conf_path="/etc/postgresql/15/main/"
         elif hostdistro.startswith("\"Alma"):
+            #Install postgresql on almalinux
             stdin, stdout, stderr = sshcon.exec_command("dnf install -y postgresql-server;  postgresql-setup --initdb ;systemctl enable postgresql.service; systemctl start postgresql.service")
             stdout.read().decode(encoding='UTF-8')
+            #Write path to conf files in variables for almalinux
             self.pg_conf_path="/var/lib/pgsql/data/"
+        #Check if installation is successfull using check psql version
         stdin, stdout, stderr = sshcon.exec_command("psql -V")
         if stderr.read().decode(encoding='UTF-8')=="":
             self.install_status = True
